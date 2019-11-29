d48d053fb495b20ab6d13bf750e8035acf1ce02a
zbynioo85 <zbynioo85@gmail.com>
2019-11-29 17:49:04 +0100

    add handeInputChange

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import 'weather-icons/css/weather-icons.css'
+import React, { Component } from "react";
+import "./App.css";
+import "weather-icons/css/weather-icons.css";
+import Form from "./Form";
 class App extends Component {
+  state = {
+    value: "",
+    err: false
+  };
+  handelInputChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
   render() {
     return (
       <div className="App">
+        <Form value={this.state.value} change={this.handelInputChange}></Form>
         <i className=" wi wi-day-sunny"></i>
       </div>
     );
