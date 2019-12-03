90983d21edc12146ac1b27d7675d4800d4a4dbaa
zbynioo85 <zbynioo85@gmail.com>
2019-12-03 01:20:55 +0100

    styleing app

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './Info.css'
 const Info = props => {
   return (
     <div className="info">
-      <i className={`wi ${props.icon}`}></i>
-      <span>{props.text}</span>
+      <i className={` info__icon wi ${props.icon}`}></i>
+      <span className='info__text'>{props.text}</span>
     </div>
   );
 };
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Info from "./Info";
+import "./Result.css";
 
 const Result = props => {
   const {
@@ -22,27 +23,24 @@ const Result = props => {
     const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString();
     const sunsetTime = new Date(sunset * 1000).toLocaleTimeString();
     content = (
-      <>
+      <div className="result">
         <h1>
           {city.toUpperCase()}, {country}
         </h1>
-        <i className={`wi ${icon}`}></i>
-        <p>{Math.round(temp)}&#176;C</p>
-        <span>{Math.round(minTemp)}&#176;C</span>
-        <span>{Math.round(maxTemp)}&#176;C</span>
-
-        <p>{description}</p>
-        <details>
-          <summary>More info</summary>
+        <i className={`result__main-icon wi ${icon}`}></i>
+        <p className="result__main-temp">{Math.round(temp)}&#176;C</p>
+        <span className="result__min-temp">{Math.round(minTemp)}&#176;C</span>
+        <span className="result__max-temp">{Math.round(maxTemp)}&#176;C</span>
+        <p className='result__description'>{description}</p>
+        <details >
+          <summary className='result__btn'>More info</summary>
           <Info icon={"wi-time-3"} text={date}></Info>
           <Info icon={"wi-barometer"} text={`${pressure}  hpa`}></Info>
           <Info icon={"wi-windy"} text={`${wind} m/s`}></Info>
-
           <Info icon={"wi-sunrise"} text={sunriseTime}></Info>
-
           <Info icon={"wi-sunset"} text={sunsetTime}></Info>
         </details>
-      </>
+      </div>
     );
   }
   return <div className="result">{err ? `Not Found ${city}` : content}</div>;
